a83035b6587f6b5d0305d100170f2cd67381506b
samichelly <sami.chelly15@gmail.com>
2024-01-29 19:43:42 +0100

    Refactoring app into 3 apps (v1) + adapting db for each app

---
+++ oc_lettings_site/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("lettings/", include("lettings.urls")),
+    path("profiles/", include("profiles.urls")),
+    path("admin/", admin.site.urls),
+]
