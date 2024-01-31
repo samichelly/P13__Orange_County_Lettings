6b1ab647bec2f7001acb94a5b6cb39ba88006bc5
samichelly <sami.chelly15@gmail.com>
2024-01-31 21:46:22 +0100

    Add sentry test + no fix bug static repo

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls import handler404, handler500
 from . import views
 
 
-# def trigger_error(request):
-#     division_by_zero = 1 / 0
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 
 handler404 = "oc_lettings_site.views.custom_404"
@@ -16,6 +16,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
-    # path("sentry-debug/", trigger_error),
+    path("sentry-debug/", trigger_error),
     path("admin/", admin.site.urls),
 ]
