a780852deb9cbdbbf5369f039cdda172bab355c1
samichelly <sami.chelly15@gmail.com>
2024-02-04 00:16:48 +0100

    add tests

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Address, Letting
+
+
+class LettingsTest(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number=20,
+            street="rue des Merisiers",
+            city="Versailles",
+            state="Yvelines",
+            zip_code=78000,
+            country_iso_code="FRA",
+        )
+        self.letting = Letting.objects.create(
+            title="Test Domain Versailles", address=self.address
+        )
+
+    def test_lettings_index(self):
+        response = self.client.get(reverse("lettings:index"))
+        assert response.status_code == 200
+        assert b"<title>Lettings</title>" in response.content
+
+    def test_letting_detail(self):
+        response = self.client.get(reverse("lettings:letting", args=[1]))
+        assert response.status_code == 200
+        assert b"<title>Test Domain Versailles</title>" in response.content
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
