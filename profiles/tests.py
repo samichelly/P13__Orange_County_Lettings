33fc5cf917c5f35a68387ce56a44e86ea2e1de80
samichelly <sami.chelly15@gmail.com>
2024-02-04 00:16:48 +0100

    add tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Profile
+
+
+class ProfilesTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="TestUser", password="password890", email="test_user@mail.com"
+        )
+        self.profile = Profile.objects.create(user=self.user, favorite_city="Rio")
+
+    def test_profiles_index(self):
+        response = self.client.get(reverse("profiles:index"))
+        assert response.status_code == 200
+        assert b"<title>Profiles</title>" in response.content
+
+    def test_profile_detail(self):
+        response = self.client.get(reverse("profiles:profile", args=["TestUser"]))
+        assert response.status_code == 200
+        assert b"<title>TestUser</title>" in response.content
