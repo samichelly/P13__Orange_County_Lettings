4b64142802b67a49cbe4e97eda7d4c9296d9a3a2
samichelly <sami.chelly15@gmail.com>
2024-02-04 15:03:26 +0100

    add docstrings for tests + sentry capture message for 404/500 errors

--- lettings/tests.py
+++ lettings/tests.py
@@ -5,7 +5,21 @@ from .models import Address, Letting
 
 
 class LettingsTest(TestCase):
+    """
+    TestCase class for testing Lettings app functionality.
+
+    Methods:
+        setUp: Set up initial data for tests.
+        test_lettings_index: Test the index view of the Lettings app.
+        test_letting_detail: Test the detail view of a letting in the Lettings app.
+    """
+
     def setUp(self):
+        """
+        Set up initial data for tests.
+
+        Creates an Address and a Letting for testing.
+        """
         self.address = Address.objects.create(
             number=20,
             street="rue des Merisiers",
@@ -19,11 +33,21 @@ class LettingsTest(TestCase):
         )
 
     def test_lettings_index(self):
+        """
+        Test the index view of the Lettings app.
+
+        Asserts that the response status code is 200 and contains the expected title.
+        """
         response = self.client.get(reverse("lettings:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, b"<title>Lettings</title>")
 
     def test_letting_detail(self):
+        """
+        Test the detail view of a letting in the Lettings app.
+
+        Asserts that the response status code is 200 and contains the expected title.
+        """
         response = self.client.get(reverse("lettings:letting", args=[1]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, b"<title>Test Domain Versailles</title>")
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -3,7 +3,19 @@ from django.urls import reverse
 
 
 class IndexTests(TestCase):
+    """
+    TestCase class for testing the index view of the oc_lettings_site app.
+
+    Methods:
+        test_index_page_status_code: Test the status code of the index page.
+    """
+
     def test_index_page_status_code(self):
+        """
+        Test the status code of the index page.
+
+        Asserts that the response status code is 200 for the index page.
+        """
         url = reverse("index")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
